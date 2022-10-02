02e06e80d817ce48f73a7979e8b96609af5ecd79
Hayley Mead <Hayleymead3@gmail.com>
2022-10-02 13:17:02 -0600

    added minor style

--- my-app/src/Components/Login/user-Login-form.js
+++ my-app/src/Components/Login/user-Login-form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../../shared/css/loginForm.css';
 import * as Yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { FormGroup, Button, NavLink } from "react-bootstrap";
@@ -14,23 +15,18 @@ const UserLoginForm = (props) => {
   const validationSchema = Yup.object().shape({
     // password: Yup.string().required("Required"),
   });
-
-
-
-
   console.log(props);
   return (
 
-    
-    
-    <div className="form-group">
-      <h2 classname="first-header">Welcome Back!</h2>
+    <div className="form-floating mb-3">
+      <h2 className="first_header">Welcome Back!</h2>
       <Formik {...props} validationSchema={validationSchema}>
         <Form>
           <FormGroup>
-            <label htmlFor="login_email_feild">Email: </label>
+            {/* <label htmlFor="login_email_feild">Email: </label> */}
             <Field
               id="email"
+              placeholder="Email"
               name="login_email_feild"
               type="text"
               className="form-control"
@@ -40,9 +36,10 @@ const UserLoginForm = (props) => {
               className="d-block invalid-feedback"
               component={"span"}
             />
-            <label htmlFor="login_password_feild">Password: </label>
+            {/* <label htmlFor="login_password_feild">Password: </label> */}
             <Field
               id="password"
+              placeholder="Password"
               name="login_password_feild"
               type="password"
               className="form-control"
@@ -53,21 +50,24 @@ const UserLoginForm = (props) => {
               component={"span"}
             />
           </FormGroup>
-          <Button
-            variant="danger"
-            size="lg"
-            block="block"
-            // onClick={""}
-            type="submit"
-          >
-            {props.children}
-          </Button>
+          <div className="button_login">
+            <Button 
+              variant="primary"
+              size="lg"
+              block="block"
+              //onClick={"/forum"}
+              type="submit"
+              >
+              {props.children}
+            </Button>
+          </div>
+
 
           <div className="login_links">
-            <p>
-              <Link to={"/create-user"}>register now</Link>
+            <p className="register_link">
+              <Link to={"/create-user"}>Register Now</Link>
             </p>
-            <p>
+            <p className="forgot_link">
               <Link to={""}>Forgot Password?</Link>
             </p>
           </div>
